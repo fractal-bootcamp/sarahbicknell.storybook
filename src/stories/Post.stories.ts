015b56a91a5553156ab5ea0150a5571a79d9fe13
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-06-06 18:28:59 -0400

    updated post component to accept props instead of hardcoded values:

--- src/stories/Post.stories.ts
+++ src/stories/Post.stories.ts
@@ -1,6 +1,6 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { fn } from '@storybook/test';
-import Post from './Post';
+import Post, {PostProps} from './Post';
 
 const meta = {
   title: "Post",
@@ -10,5 +10,17 @@ const meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+const post: PostProps = {
+  username: "Helena",
+  avatarUrl: "https://s3-alpha-sig.figma.com/img/48fb/0979/b0fbdd8e320622de39475b562ddad56d?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gcuGV23RG8rVq1PMQPkSLmqZmYBkCAgeW0JY-SkVMG7jp4Ye~eii5l0sgfMuYwLqkwPMSgVIZXUX6wz3d-jiXHxGKSlvkqwY7rMSNFPK~kFjK6Qovpzue3yLdxx9n5I7gugFUAPhlmBFqeRQZ3uHV9n0YbKMZBfdUv3pENP8qC14abVvddt~qvYQFaPmxhVv9xb2BAXv5yCIj61X6vr0YpxVhAO8-iThs5s9YdwahRw9t7~AmycNAuVmO9fk2wdlHn8p20fgV6QeoQNkvl-K-nSrVJWLbvDAS6A4l9xf2Udm2gIl8REAvakXNBjat0xbNIQfUvsqhY2tKbHC6NSX7g__",
+  group: "Group name",
+  timestamp: 0,
+  thumbnailUrl: "https://s3-alpha-sig.figma.com/img/e708/8392/7f135f87cd002a7122bb6b6bd3722bc6?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nzZ-qWJfFOpviBdvFOOC~QzkhhDeI-tQ5aPXQGP8pWVjdrp~8MLtOgz9K6z5lrtJAqx3l~~DmWKstP1IjH3QxdHQIvhTD2-W1k5GVNd08o19THS7uzRUhsrWqi6MuzJb1ikHl8rEyz7cOyzYSIUvtojJ-5VOH6xMKgCAa0zEjjR9OKFtG-ves8Tz8N6qq3XnrMbWOPeyXtalhCgNbvdccFeBaoi4AmhG3iLuXtM-A1sTSmuuewf7N4HRF7jh~UrXfoNxuEJC~nJgC7rQX-zOwDWN~ZYdfzhkJOez86iE4F19Gy7Yr~nH4RhjwaQQQjJv7~Mh7Lt91-~dK54LiqqNvA__",
+  description: "Post description",
+  likes: 21,
+  comments: 4
+}
+
 export const Default: Story = {
+  args: post
 };
--- src/stories/Post.tsx
+++ src/stories/Post.tsx
@@ -4,22 +4,34 @@ import '../index.css'
 // // import { fab } from '@fortawesome/free-brands-svg-icons'
 // import { far } from '@fortawesome/free-regular-svg-icons'
 
-const Post = () => {
+
+export type PostProps = {
+    username: string, 
+    avatarUrl: string, 
+    group: string, 
+    timestamp: number, 
+    thumbnailUrl: string, 
+    description: string, 
+    likes: number, 
+    comments: number
+}
+
+const Post = ({username, avatarUrl, group, timestamp, thumbnailUrl, description, likes, comments}: PostProps) => {
     return ( 
         <div className="flex"> 
             <div className='flex flex-col px-4'>
-                <img className="w-[48px] h-[48px] rounded-full" src="https://s3-alpha-sig.figma.com/img/48fb/0979/b0fbdd8e320622de39475b562ddad56d?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=gcuGV23RG8rVq1PMQPkSLmqZmYBkCAgeW0JY-SkVMG7jp4Ye~eii5l0sgfMuYwLqkwPMSgVIZXUX6wz3d-jiXHxGKSlvkqwY7rMSNFPK~kFjK6Qovpzue3yLdxx9n5I7gugFUAPhlmBFqeRQZ3uHV9n0YbKMZBfdUv3pENP8qC14abVvddt~qvYQFaPmxhVv9xb2BAXv5yCIj61X6vr0YpxVhAO8-iThs5s9YdwahRw9t7~AmycNAuVmO9fk2wdlHn8p20fgV6QeoQNkvl-K-nSrVJWLbvDAS6A4l9xf2Udm2gIl8REAvakXNBjat0xbNIQfUvsqhY2tKbHC6NSX7g__" /> 
+                <img className="w-[48px] h-[48px] rounded-full" src={avatarUrl} /> 
             </div>
             <div>
                 <div className="flex text-lg">
-                    <p> Helena </p>
-                    <p className="text-slate-400 font-light "> &#8205;  in Group name</p>
+                    <p> {username} </p>
+                    <p className="text-slate-400 font-light "> &#8205;  in {group}</p>
                 </div>
                 <div>
-                    <p className="text-slate-400 text-md font-light pb-4 "> 0 min ago </p>
-                    <img className='w-[458px] h-[458px] rounded pb-4' src="https://s3-alpha-sig.figma.com/img/e708/8392/7f135f87cd002a7122bb6b6bd3722bc6?Expires=1718582400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=nzZ-qWJfFOpviBdvFOOC~QzkhhDeI-tQ5aPXQGP8pWVjdrp~8MLtOgz9K6z5lrtJAqx3l~~DmWKstP1IjH3QxdHQIvhTD2-W1k5GVNd08o19THS7uzRUhsrWqi6MuzJb1ikHl8rEyz7cOyzYSIUvtojJ-5VOH6xMKgCAa0zEjjR9OKFtG-ves8Tz8N6qq3XnrMbWOPeyXtalhCgNbvdccFeBaoi4AmhG3iLuXtM-A1sTSmuuewf7N4HRF7jh~UrXfoNxuEJC~nJgC7rQX-zOwDWN~ZYdfzhkJOez86iE4F19Gy7Yr~nH4RhjwaQQQjJv7~Mh7Lt91-~dK54LiqqNvA__" />
-                    <p className='text-2xl font-light pb-2'> Post description </p> 
-                    <p className='font-semibold'> 21 likes 4 comments </p> 
+                    <p className="text-slate-400 text-md font-light pb-4 "> {timestamp} min ago </p>
+                    <img className='w-[458px] h-[458px] rounded pb-4' src={thumbnailUrl} />
+                    <p className='text-2xl font-light pb-2'> {description} </p> 
+                    <p className='font-semibold'> {likes} likes {comments} comments </p> 
                 </div>
             </div>
 
